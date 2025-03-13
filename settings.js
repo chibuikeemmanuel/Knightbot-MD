7e44847c1e0193a7b2ab58e5f455fd9a52d4dea1
CHIBUIKE OSY-AGBATA <ositaagbatachibuike@gmail.com>
2025-03-13 15:10:53 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "METRO_MD",
+  botOwner: 'METRO', // Your name
+  ownerNumber: '2349036164643', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
